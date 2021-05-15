b607b9dc7c9151b535aca98a0ded9ae22805161b
gavinhome <yangxiaoming512@yeah.net>
2021-05-15 21:05:10 +0800

    add counter test

--- tests/Counter.spec.ts
+++ tests/Counter.spec.ts
@@ -3,7 +3,7 @@ import Counter from '../src/views/Counter.vue'
 
 test('Counter.vue', async () => {
   const wrapper = mount(Counter)
-  expect(wrapper.html()).toContain('Unit Test Page')
+  expect(wrapper.html()).toContain('Counter Page')
   expect(wrapper.html()).toContain('count is: 0')
   await wrapper.find('button').trigger('click')
 })

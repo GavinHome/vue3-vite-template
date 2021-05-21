fc7be361c85524c69bfc284cc3fecf004ba19c79
GavinHome <yangxiaoming512@yeah.net>
2021-05-21 14:10:16 +0800

    update

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,13 +2,12 @@ import { ApiConst } from '../common/const'
 import { ApiResult, UserAuthModel } from '../common/model'
 import { useAxios } from '../hook'
 
-const apiRefreshToken = (): Promise<ApiResult<UserAuthModel>> => {
-  return useAxios().post<ApiResult<UserAuthModel>>(`${ApiConst.API_REFRESH_TOKEN}`)
-}
+const apiRefreshToken = (): Promise<ApiResult<UserAuthModel>> =>
+  useAxios().post<ApiResult<UserAuthModel>>(`${ApiConst.API_REFRESH_TOKEN}`)
 
-const apiAxiosUser = () => useAxios().get(ApiConst.API_AXIOS_USER, {}, false)
+const apiAxiosUser = (): Promise<any> => useAxios().get(ApiConst.API_AXIOS_USER, {}, false)
 
-const apiLogError = (bodyContent: any) =>
+const apiLogError = (bodyContent: object): Promise<ApiResult<any>> =>
   useAxios().post<ApiResult<any>>(`${ApiConst.API_LOG_INDEX}`, bodyContent)
 
 export { apiRefreshToken, apiAxiosUser, apiLogError }

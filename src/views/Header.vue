db006369dfd0fb2246efec0de53bb4c66ef75a72
GavinHome <yangxiaoming512@yeah.net>
2021-05-22 13:27:41 +0800

    update header setup

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -1,10 +1,10 @@
 <template>
-  <a-menu mode="horizontal" :selectedKeys="state.selectedKeys" @click="click">
-    <a-menu-item v-for="item in state.menus" :key="item.key">
+  <a-menu mode="horizontal" :selectedKeys="selectedKeys" @click="click">
+    <a-menu-item v-for="item in navigators" :key="item.key">
       {{ item.title }}
     </a-menu-item>
-    <div class="select-lang">
-      <a-select default-value="en-US" @change="handleLangChange">
+    <div class="lang">
+      <a-select default-value="en-US" @change="switchLangChange">
         <a-select-option value="en-US"> {{ t('LOCALE_EN') }} </a-select-option>
         <a-select-option value="zh-CN"> {{ t('LOCALE_ZH') }} </a-select-option>
       </a-select>
@@ -13,7 +13,7 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, reactive, watch, computed } from 'vue'
+import { onMounted, reactive, watch, computed, ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { useI18n } from 'vue-i18n'
 import RouterConst from '../router/const'
@@ -21,25 +21,24 @@ import RouterConst from '../router/const'
 const router = useRouter()
 const { t } = useI18n()
 const i18n = useI18n()
-const menus = computed(() => [
+const navigators = computed(() => [
   { key: RouterConst.ROUTER_HOME, title: t('ROUTER_HOME') },
   { key: RouterConst.ROUTER_ABOUT, title: t('ROUTER_ABOUT') },
   { key: RouterConst.ROUTER_COUNTER, title: t('ROUTER_COUNTER') },
   { key: RouterConst.ROUTER_AXIOS, title: t('ROUTER_AXIOS') }
 ])
 
-const selectedKeys: Array<string> = []
+const selectedKeys = ref<Array<string>>([])
 const click = async (params: { key: string }) => {
   await router.push({ name: params.key })
 }
-const state = reactive({ selectedKeys, menus })
 
 const activeNav = (path: string) => {
   const name = path.replace('/', '')
-  if (state.menus.find((x) => x.key === name)) {
-    state.selectedKeys = [name]
+  if (navigators.value.find((x) => x.key === name)) {
+    selectedKeys.value = [name]
   } else {
-    state.selectedKeys = [RouterConst.ROUTER_HOME]
+    selectedKeys.value = [RouterConst.ROUTER_HOME]
   }
 }
 
@@ -56,13 +55,13 @@ onMounted(() => {
   })
 })
 
-const handleLangChange = (value: string) => {
+function switchLangChange(value: string) {
   i18n.locale.value = value
 }
 </script>
 
 <style lang="stylus" scoped>
-.select-lang {
+.lang {
   float: right;
   margin-right: 20px;
 }
